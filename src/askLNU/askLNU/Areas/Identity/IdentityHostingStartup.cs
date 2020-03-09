e9aa42a4ff90756424718957be2c2ca835d2d44d
Maksym Dovhan <kerman901@gmail.com>
2020-03-09 19:32:39 +0200

    Add role to user during registration

---
+++ src/askLNU/askLNU/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using askLNU.Data;
+using askLNU.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(askLNU.Areas.Identity.IdentityHostingStartup))]
+namespace askLNU.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
