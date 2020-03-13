047b0808cf61b262df9e4b9f1864d302956a7413
Maksym Dovhan <kerman901@gmail.com>
2020-03-14 00:58:47 +0200

    FIx home controller

--- src/askLNU/askLNU/Controllers/HomeController.cs
+++ src/askLNU/askLNU/Controllers/HomeController.cs
@@ -31,8 +31,7 @@ namespace askLNU.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            // return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
