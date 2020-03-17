ce1dbcc9f58fdf45ffc55b9430c1ece556b49782
ksenyaklakovych <kklakovych@gmail.com>
2020-03-17 18:55:30 +0200

    Created Account Confirmation (#12)

---
+++ src/askLNU/askLNU.BLL/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace askLNU.BLL.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
