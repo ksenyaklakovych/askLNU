5e38f8c0cc3b9f939f52cdeb3961eceb1b42acd6
ksenyaklakovych <kklakovych@gmail.com>
2020-03-19 21:25:38 +0200

    Added test methos, updated UserDTO

--- src/askLNU/askLNU.Tests/UserServiceTest.cs
+++ src/askLNU/askLNU.Tests/UserServiceTest.cs
@@ -14,13 +14,12 @@ namespace askLNU.Tests
 {
     public class UserServiceTest
     {
-        [Fact]
-        public void GetByEmailAsync_ShouldReturnTrue()
+        private Mock<UserManager<ApplicationUser>> fakeManager;
+        private Mock<ILogger<UserService>> _logger;
+
+        public UserServiceTest()
         {
-            string email = "test";
-            //Arrange
-            var fakeUser = Task.Run(() => new ApplicationUser("TestName","TestSurname",1,false,"image.jpg"));
-            var fakeManager = new Mock<UserManager<ApplicationUser>>(
+            fakeManager = new Mock<UserManager<ApplicationUser>>(
                     new Mock<IUserStore<ApplicationUser>>().Object,
                     new Mock<IOptions<IdentityOptions>>().Object,
                     new Mock<IPasswordHasher<ApplicationUser>>().Object,
@@ -30,15 +29,66 @@ namespace askLNU.Tests
                     new Mock<IdentityErrorDescriber>().Object,
                     new Mock<IServiceProvider>().Object,
                     new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+            _logger = new Mock<ILogger<UserService>>();
+        }
+        [Fact]
+        public void GetByEmailAsync_WithCorrectEmail_ShouldReturnTrue()
+        {
+            string email = "test";
+            //Arrange
+            var fakeUser = Task.Run(() => new ApplicationUser("TestName","TestSurname",1,false,"image.jpg"));
             fakeManager.Setup(m => m.FindByEmailAsync(email)).Returns(fakeUser);
             
             UserService userService = new UserService(fakeManager.Object);
             //Act
             var result = userService.GetByEmailAsync(email);
+
             //Assert
             Assert.Equal("TestName",result.Result.Name);
             Assert.Equal("TestSurname", result.Result.Surname);
             Assert.False(result.Result.IsBlocked);
         }
+        [Fact]
+        public void GetByEmailAsync_WithIncorrectEmail_ShouldReturnTrue()
+        {
+            string email = "test";
+            //Arrange
+            var fakeUser = Task.Run(() => new ApplicationUser());
+            fakeManager.Setup(m => m.FindByEmailAsync(email)).Returns(Task.FromResult((ApplicationUser)null));
+
+            UserService userService = new UserService(fakeManager.Object);
+            //Act
+            var result = userService.GetByEmailAsync(email);
+
+            //Assert
+            Assert.Null(result.Result);
+        }
+
+        //[Fact]
+        //public void CreateUserAsync_ShouldReturnTrue()
+        //{
+        //    //Arrange
+        //    string password = "password";
+        //    var fakeUserDTO = new UserDTO("testUsername", "TestName", "TestSurname", 1, false, "image.jpg", "test@gmail.com");
+        //    var fakeAppUser = new ApplicationUser()
+        //    {
+        //        Name = fakeUserDTO.Name,
+        //        Surname = fakeUserDTO.Surname,
+        //        Course = fakeUserDTO.Course,
+        //        FacultyId = fakeUserDTO.FacultyId,
+        //        ImageSrc = fakeUserDTO.ImageSrc,
+        //        UserName = fakeUserDTO.UserName,
+        //        Email = fakeUserDTO.Email
+        //    };
+        //    fakeManager.Setup(m => m.CreateAsync(fakeAppUser, password)).ReturnsAsync(IdentityResult.Success);
+        //    UserService userService = new UserService(fakeManager.Object);
+            
+        //    //Act
+        //    var result = userService.CreateUserAsync(fakeUserDTO, password);
+
+        //    //Assert
+        //    Console.WriteLine(result.Result);
+        //}
+
     }
 }
