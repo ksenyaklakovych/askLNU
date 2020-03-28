93004ccf47a2006acc495d3ec0498d727625c662
KhrystynaPavlikovska <KhrystynaPavlikovska@users.noreply.github.com>
2020-03-28 15:40:30 +0200

    add profile page

---
+++ src/askLNU/askLNU/Controllers/UserProfileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using askLNU.ViewModels;
+
+
+namespace askLNU.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/askLNU/askLNU/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace askLNU.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Course { get; set; }
+        public string ImageSrc { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+    }
+}
