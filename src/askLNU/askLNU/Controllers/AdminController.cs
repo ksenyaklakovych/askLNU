6f43d90eb362334d599bddb01f8cbadf2ba46cc6
Ksenia Klakovych <kklakovych@gmail.com>
2020-04-07 18:41:47 +0300

    Started Users page for admin

---
+++ src/askLNU/askLNU/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using askLNU.BLL.DTO;
+using askLNU.BLL.Interfaces;
+using askLNU.DAL.Entities;
+using askLNU.InputModels;
+using askLNU.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace askLNU.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<AdminController> _logger;
+        private readonly IUserService _userService;
+
+
+        public AdminController(ILogger<AdminController> log, 
+            UserManager<ApplicationUser> userManager,
+            IUserService service)
+        {
+            _logger = log;
+            _userManager = userManager;
+            _userService = service;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index(string email)
+        {
+
+            return View();
+        }
+        
+       
+    }
+}
---
+++ src/askLNU/askLNU/ViewModels/UserForAdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace askLNU.ViewModels
+{
+    public class UserForAdminViewModel
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool IsModerator { get; set; }
+    }
+}
