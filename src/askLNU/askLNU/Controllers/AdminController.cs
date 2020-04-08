f10abca618792cb56536a41c51d7d3d2f16ef89b
AndriyPlekan <44374776+AndriyPlekan@users.noreply.github.com>
2020-04-08 17:42:15 +0300

    Created page AllFaculties for admin, added function to CreateFaculty, added buttton"Faculty" in main page for Admin

--- src/askLNU/askLNU/Controllers/AdminController.cs
+++ src/askLNU/askLNU/Controllers/AdminController.cs
@@ -20,17 +20,25 @@ namespace askLNU.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<AdminController> _logger;
         private readonly IUserService _userService;
+        private readonly IFacultyService _facultyService;
+
         private Mapper _mapper;
+        private Mapper _mapperFaculty;
+
 
         public AdminController(ILogger<AdminController> log,
             UserManager<ApplicationUser> userManager,
-            IUserService service)
+            IUserService service,
+            IFacultyService facultyService)
         {
             _logger = log;
             _userManager = userManager;
             _userService = service;
+            _facultyService = facultyService;
             var config = new MapperConfiguration(cfg => cfg.CreateMap<UserDTO, UserForAdminViewModel>());
+            var config2 = new MapperConfiguration(cfg => cfg.CreateMap<FacultyDTO, FacultyViewModel>());
             _mapper = new Mapper(config);
+            _mapperFaculty = new Mapper(config2);
         }
 
         [Authorize(Roles = "Admin")]
@@ -65,6 +73,22 @@ namespace askLNU.Controllers
             return RedirectToAction("Index");
         }
 
-
+        public ActionResult AllFaculties()
+        {
+            _logger.LogInformation("Display all faculties");
+            var all_faculties= _mapperFaculty.Map<IEnumerable <FacultyViewModel>>(_facultyService.GetAll());
+            return View(all_faculties);
+        }
+        public ActionResult CreateNewFaculty()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateNewFaculty(FacultyViewModel faculty)
+        {
+            FacultyDTO facultyDTO = new FacultyDTO { Title = faculty.Title };
+            _facultyService.CreateFaculty(facultyDTO);
+            return RedirectToAction("AllFaculties");
+        }
     }
 }
---
+++ src/askLNU/askLNU/ViewModels/FacultyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace askLNU.ViewModels
+{
+    public class FacultyViewModel
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
