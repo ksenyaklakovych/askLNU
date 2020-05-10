8228fecc7f51ece905c14516f3f1fd17b6599d27
KhrystynaPavlikovska <34419998+KhrystynaPavlikovska@users.noreply.github.com>
2020-05-11 01:12:55 +0300

    Unite user pages branch (#60)
    * added photo on ../UserProfile page
    
    + created ../UserProfile/ChangePhoto page
    
    * added links to 'FavoriteQuestions' and 'CreatedQuestions' pages
    
    * Changed controller for UserProfile
    
    * changed front end to all pages
    
    Co-authored-by: KhrystynaPavlikovska <KhrystynaPavlikovska@users.noreply.github.com>
    Co-authored-by: Ksenia Klakovych <kklakovych@gmail.com>

--- src/askLNU/askLNU/Controllers/UserProfileController.cs
+++ src/askLNU/askLNU/Controllers/UserProfileController.cs
@@ -7,20 +7,32 @@
     using askLNU.DAL.Entities;
     using askLNU.ViewModels;
     using AutoMapper;
+    using askLNU.BLL.DTO;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using askLNU.BLL.Interfaces;
+
+
+
 
     public class UserProfileController : Controller
     {
+        private readonly IImageService _imageService;
+        private readonly IUserService _userService;
         private readonly UserManager<ApplicationUser> _userManager;
         private Mapper _mapper;
 
         public UserProfileController(
-           UserManager<ApplicationUser> userManager)
+           UserManager<ApplicationUser> userManager,
+            IUserService userService,
+            IImageService imageService
+           )
         {
             this._userManager = userManager;
             var config = new MapperConfiguration(cfg => cfg.CreateMap<ApplicationUser,UserProfileViewModel>());
             this._mapper = new Mapper(config);
+            this._userService = userService;
+            this._imageService = imageService;
         }
 
         [HttpGet]
@@ -33,18 +45,43 @@
 
         [HttpPost]
         public async Task<IActionResult> Index(UserProfileViewModel user)
-        {
+        { 
             var userCurrent = await this._userManager.GetUserAsync(this.User);
             userCurrent.Name = user.Name;
             userCurrent.Surname = user.Surname;
             userCurrent.Email = user.Email;
             userCurrent.Course = user.Course;
-            userCurrent.ImageSrc = user.ImageSrc;
             userCurrent.UserName = user.UserName;
 
             var updatedUser = await this._userManager.UpdateAsync(userCurrent);
             var userModel = this._mapper.Map<UserProfileViewModel>(userCurrent);
+
             return this.View(userModel);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Profile (UserProfileViewModel userProfileViewModel)
+        //{
+        //    var user = new UserDTO
+        //    {
+        //        UserName = userProfileViewModel.UserName,
+        //        Email = userProfileViewModel.Email,
+        //        Name = userProfileViewModel.Name,
+        //        Surname = userProfileViewModel.Surname,
+        //        Course = userProfileViewModel.Course,
+        //    };
+        //    var imageSrc = await this._imageService.SaveImage(userProfileViewModel.Image);
+        //    await this._userService.UpdateImage(user.Id, imageSrc);
+        //    return this.View(imageSrc);
+        //}
+
+        public async Task<IActionResult> ChangePhoto (ChangePhotoViewModel changePhotoViewModel)
+        {
+            var userCurrent = await this._userManager.GetUserAsync(this.User);
+            var imageSrc = await this._imageService.SaveImage(changePhotoViewModel.Image);
+            await this._userService.UpdateImage(userCurrent.Id, imageSrc);
+
+            return this.View(imageSrc);
         }
     }
 }
